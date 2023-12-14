b8a4e8bbd495f5333b685796789d8caf093ba449
Michele Cazzola <michele.cazzola2407@gmail.com>
2023-12-14 17:10:14 +0100

    Lab 9 - Ex.2: find out right time periods for interrupt precedence

---
+++ Lab9/es2/sample.c
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------
+ * Name:    sample.c
+ * Purpose: 
+ *		to control led11 and led 10 through EINT buttons (similarly to project 03_)
+ *		to control leds9 to led4 by the timer handler (1 second - circular cycling)
+ * Note(s): this version supports the LANDTIGER Emulator
+ * Author: 	Paolo BERNARDI - PoliTO - last modified 15/12/2020
+ *----------------------------------------------------------------------------
+ *
+ * This software is supplied "AS IS" without warranties of any kind.
+ *
+ * Copyright (c) 2017 Politecnico di Torino. All rights reserved.
+ *----------------------------------------------------------------------------*/
+
+                  
+#include <stdio.h>
+#include "LPC17xx.H"                    /* LPC17xx definitions                */
+#include "led/led.h"
+#include "button_EXINT/button.h"
+#include "timer/timer.h"
+
+/* Led external variables from funct_led */
+extern unsigned char led_value;					/* defined in lib_led								*/
+#ifdef SIMULATOR
+extern uint8_t ScaleFlag; // <- ScaleFlag needs to visible in order for the emulator to find the symbol (can be placed also inside system_LPC17xx.h but since it is RO, it needs more work)
+#endif
+
+unsigned int IntCount;
+unsigned int X = 0x0FFFFFFF;
+
+/*----------------------------------------------------------------------------
+  Main Program
+ *----------------------------------------------------------------------------*/
+int main (void) {
+  	
+	SystemInit();  												/* System Initialization (i.e., PLL)  */
+  LED_init();                           /* LED Initialization                 */
+  BUTTON_init();												/* BUTTON Initialization              */
+	
+	IntCount = 0;
+	init_timer(3, 0x02FAF080);						/* TIMER 3 every 2 s at 25 MHz */
+	init_timer(2, 0x00BEBC20);						/* TIMER 2 every 0.5 s at 25 MHz */
+	
+	enable_timer(3);
+	enable_timer(2);
+	
+	LPC_SC->PCON |= 0x1;									/* power-down	mode										*/
+	LPC_SC->PCON &= 0xFFFFFFFFD;						
+		
+  while (1) {                           /* Loop forever                       */	
+		__ASM("wfi");
+  }
+
+}
